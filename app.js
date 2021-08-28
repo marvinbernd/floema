42fe26fcbf33048d1dadfd3932e336e0adc52be8
Marvin Bernd <hello@marvinbernd.de>
2021-08-28 20:19:28 +0200

    add pug templates and express backend

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+  res.render('pages/home')
+})
+
+app.get('/about', (req, res) => {
+  res.render('pages/about')
+})
+
+app.get('/collections', (req, res) => {
+  res.render('pages/collections')
+})
+
+app.get('/detail/:id', (req, res) => {
+  res.render('pages/detail')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
