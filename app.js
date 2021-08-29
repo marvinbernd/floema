13795a6de4b3359531211dc63efd219c7c7613db
Marvin Bernd <hello@marvinbernd.de>
2021-08-29 18:09:16 +0200

    integrate Prismic and create about page

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const port = 3000
+
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('prismic-dom')
+
+const initApi = (req) => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req
+  })
+}
+
+const handleLinkResolver = (doc) => {
+  /* // Define the url depending on the document type
+    if (doc.type === 'page') {
+      return '/page/' + doc.uid;
+    } else if (doc.type === 'blog_post') {
+      return '/blog/' + doc.uid;
+    } */
+
+  // Default to homepage
+  return '/'
+}
+
+// Middleware to inject prismic context
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver
+  }
+  // add PrismicDOM in locals to access them in templates.
+  res.locals.PrismicDOM = PrismicDOM
+  next()
+})
 
 app.set('view engine', 'pug')
 
@@ -9,7 +44,22 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-  res.render('pages/about')
+  initApi(req).then((api) => {
+    api
+      .query(Prismic.Predicates.any('document.type', ['meta', 'about']))
+      .then((response) => {
+        const { results } = response
+        const [meta, about] = results
+
+        console.log(meta, about)
+
+        // response is the response object. Render your views here.
+        res.render('pages/about', {
+          about,
+          meta
+        })
+      })
+  })
 })
 
 app.get('/collections', (req, res) => {
