5e5657147df08dd9d4c63d579a711c4095b5faf9
Marvin Bernd <hello@marvinbernd.de>
2021-08-31 22:44:28 +0200

    finalize prismic integration

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 
 const express = require('express')
+const errorHandler = require('errorhandler')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
 const app = express()
 const port = 3000
 
@@ -15,59 +20,125 @@ const initApi = (req) => {
 }
 
 const handleLinkResolver = (doc) => {
-  /* // Define the url depending on the document type
-    if (doc.type === 'page') {
-      return '/page/' + doc.uid;
-    } else if (doc.type === 'blog_post') {
-      return '/blog/' + doc.uid;
-    } */
+  // Define the url depending on the document type
+  if (doc.type === 'product') return `/detail/${doc.slug}`
+
+  if (doc.type === 'collections') return `/collections`
+
+  if (doc.type === 'about') return `/about`
 
   // Default to homepage
   return '/'
 }
 
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(methodOverride())
+app.use(errorHandler())
+
 // Middleware to inject prismic context
 app.use((req, res, next) => {
-  res.locals.ctx = {
+  /* res.locals.ctx = {
     endpoint: process.env.PRISMIC_ENDPOINT,
     linkResolver: handleLinkResolver
-  }
+  } */
+  res.locals.Link = handleLinkResolver
+
   // add PrismicDOM in locals to access them in templates.
   res.locals.PrismicDOM = PrismicDOM
+
+  const numbers = [
+    'One',
+    'Two',
+    'Three',
+    'Four',
+    'Five',
+    'Six',
+    'Seven',
+    'Eight',
+    'Nine',
+    'Ten'
+  ]
+
+  res.locals.Numbers = (index) => numbers[index]
+
   next()
 })
 
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-  res.render('pages/home')
-})
+const handleRequest = async (api) => {
+  const meta = await api.getSingle('meta')
+  const navigation = await api.getSingle('navigation')
+  const preloader = await api.getSingle('preloader')
 
-app.get('/about', (req, res) => {
-  initApi(req).then((api) => {
-    api
-      .query(Prismic.Predicates.any('document.type', ['meta', 'about']))
-      .then((response) => {
-        const { results } = response
-        const [meta, about] = results
+  return {
+    meta,
+    navigation,
+    preloader
+  }
+}
 
-        console.log(meta, about)
+app.get('/', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+  const home = await api.getSingle('home')
 
-        // response is the response object. Render your views here.
-        res.render('pages/about', {
-          about,
-          meta
-        })
-      })
+  const { results: collections } = await api.query(
+    Prismic.Predicates.at('document.type', 'collection')
+  )
+
+  res.render('pages/home', {
+    ...defaults,
+    collections,
+    home
   })
 })
 
-app.get('/collections', (req, res) => {
-  res.render('pages/collections')
+app.get('/about', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+  const about = await api.getSingle('about')
+
+  res.render('pages/about', {
+    ...defaults,
+    about
+  })
 })
 
-app.get('/detail/:id', (req, res) => {
-  res.render('pages/detail')
+app.get('/collections', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+  const home = await api.getSingle('home')
+
+  const { results: collections } = await api.query(
+    Prismic.Predicates.at('document.type', 'collection'),
+    {
+      fetchLinks: 'product.image'
+    }
+  )
+
+  res.render('pages/collections', {
+    ...defaults,
+    collections,
+    home
+  })
+})
+
+app.get('/detail/:uid', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+  const product = await api.getByUID('product', req.params.uid, {
+    fetchLinks: 'collection.title'
+  })
+
+  console.log(product)
+
+  res.render('pages/detail', {
+    ...defaults,
+    product
+  })
 })
 
 app.listen(port, () => {
